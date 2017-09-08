ae362f70497991bf78a54755ff742f095f03b4bc
John M. Wargo <john@johnwargo.com>
2017-09-08 08:04:11 -0400

    updated Fri 09/08/2017  8:04:11.24

--- glowing-pumpkin-feather-timer/wi-fi-config.h
+++
@@ -1,5 +0,0 @@
-// Populate the defines below with the settings for your Wi-Fi network
-// Network SSID (name)
-#define wifi_ssid ""
-// SSID password (use for WPA, or use as key for WEP)
-#define wifi_pass ""
